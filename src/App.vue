d76d5ddfc1d889ab9c78614cdbce2b4faf0a5f36
barzun <21ftt1287@student.pb.edu.bn>
2023-04-14 21:57:50 +0800

    more tailwind

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,48 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
+
+
+
+<body class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover" style="background-image:url('https://source.unsplash.com/1L71sPT5XKc');">
+
+
+
+  <div class="max-w-4xl flex items-center h-auto lg:h-screen flex-wrap mx-auto  lg:my-0 ">
+    <!--Main Col-->
+    <div id="profile" class="w-full lg:w-3/5 rounded-lg lg:rounded-l-lg lg:rounded-r-none shadow-2xl bg-white opacity-100 mx-6 lg:mx-0">
+
+        <div class="p-4 md:p-12 text-center lg:text-left">
+            <!-- Image for mobile view-->
+            <div><img src="../assets/DD.jpg" class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover "></div>
+
+            <h1 class="text-3xl font-bold pt-8 lg:pt-0">Barzun Zakwan</h1>
+            <div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-emerald-500 opacity-25"></div>
+            <p class="pt-3 text-base font-bold flex items-center justify-center lg:justify-start">Student ID: </p>
+            <p class="pt-2 text-base font-bold lg:text-sm flex items-center justify-center lg:justify-start">Group Code: DWDT07</p>
+            <p class="pt-8 text-sm">I am a student from Politeknik Brunei taking Web Development course. 
+            I am currently i my 4th semester of learning and have started doing our final year project. 
+            The project is called Market, it is an application that lets users buy, sell or browse certain items.
+            </p>
+        </div>
+
+    </div>
+
+    <!--Img Col-->
+    <div class="w-full lg:w-2/5">
+        <img src="../assets/DD.jpg" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block transform scale-105">
+    </div>
+
+</div>
+
+
+	
+	
+</body>
+
+
+  
 </template>
 
 <style scoped>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{html,js}"],
+module.exports = {
+  content: ["./index.html","./src/**/*.{html,js,vue,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
