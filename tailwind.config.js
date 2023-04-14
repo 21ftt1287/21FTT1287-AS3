5dbb4d6deca0675d87cf4b65ec7a8eacb784bea3
barzun <21ftt1287@student.pb.edu.bn>
2023-04-14 22:33:48 +0800

    config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./App.vue","./src/**/*.{html,js,vue,ts,jsx,tsx}"],
+  content: ["./index.html","./src/**/*.{html,js,vue,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
